8c01df22ddfe9d39eec936c71615abb446cca33c
JP Mac <jp@JPs-MacBook-Pro.local>
2015-08-19 22:57:16 -0700

    test123

--- client/views/chancho/chanchotodos-show.js
+++ client/views/chancho/chanchotodos-show.js
@@ -7,7 +7,7 @@ var listRenderHold = LaunchScreen.hold();
 listFadeInHold = null;
 
 var data = {
-    labels: ["January", "February", "March", "April", "May", "June", "July"],
+    labels: ["January", "February", "March", "April", "May", "June", "July", "August"],
     datasets: [
         {
             label: "My First dataset",
@@ -17,7 +17,7 @@ var data = {
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(220,220,220,1)",
-            data: [65, 59, 80, 81, 56, 55, 40]
+            data: [65, 59, 80, 81, 56, 55, 40, 12]
         },
         {
             label: "My Second dataset",
@@ -27,7 +27,7 @@ var data = {
             pointStrokeColor: "#fff",
             pointHighlightFill: "#fff",
             pointHighlightStroke: "rgba(151,187,205,1)",
-            data: [28, 48, 40, 19, 86, 27, 90]
+            data: [28, 48, 40, 19, 86, 27, 90,12]
         }
     ]
 };
